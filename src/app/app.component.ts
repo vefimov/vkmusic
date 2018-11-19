bee034016d97aec6c3c2f2bea895e5f8e1c31ec3
Valentyn Yefimov <valentine.efimov@gmail.com>
2018-11-19 19:03:05 +0200

    App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'some-app';
+  inputText = '0';
+
+  heroes = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  getFilteredHeroes() {
+    console.log('Getting heroes');
+
+    return this.heroes.filter(hero => hero.id > +this.inputText);
+  }
 }
